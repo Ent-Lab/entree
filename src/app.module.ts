55f2a8931b89b1dc02dd459b96bc2fc0f558d8ce
Ubuntu <ubuntu@ip-172-31-37-83.ap-northeast-2.compute.internal>
2022-10-04 09:59:47 +0000

    feat: 테스트 환경 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
-import { CacheModule, Logger, Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Logger, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
